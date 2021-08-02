de2e67a88b0580d4763c0c3084205f6cbb23961e
Manan Gupta <manangreen8877@gmail.com>
2021-08-02 23:06:14 +0530

    Create SearchInsertPos.java

---
+++ SearchInsertPos.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums[0]>target){
+            return 0;
+        }
+        if(nums[nums.length-1]<target){
+            return nums.length;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+            
+            if(nums[i+1]>target){
+                return i+1;
+            }
+            
+        }
+        return 0;
+    }
+}
